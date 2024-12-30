d01e441f86b2c825805c19d58a59553234b96071
aey16 <aey16@aey16s-MacBook-Air.local>
2024-12-30 16:27:52 +0700

    fix

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,4 +1,4 @@
 output "ecr_name" {
     description = "ECR Name"
-    value = awscc_ecr_repository.ecr_api_gateway.repository_name
+    value = awscc_ecr_repository.ecr_whatsapp_bill_backoffice.repository_name
 }
