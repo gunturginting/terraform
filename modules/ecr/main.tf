6bd1806b1dddff80c59ab02e13c09a20864a62b6
aey16 <aey16@aey16s-MacBook-Air.local>
2024-12-30 16:31:46 +0700

    add new ecr

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -5,3 +5,11 @@ resource "awscc_ecr_repository" "ecr_whatsapp_bill_backoffice" {
       scan_on_push = false
     }
 }
+
+resource "awscc_ecr_repository" "ecr_whatsapp_bill_payment_scheduler" {
+    repository_name = "ppob/whatsapp-bill-payment-scheduler"
+    image_tag_mutability = "MUTABLE"
+    image_scanning_configuration = {
+      scan_on_push = false
+    }
+}
