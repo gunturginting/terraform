be707ffcf48e29026f9ac96f1b91c5ca82a996e7
aey16 <aey16@aey16s-MacBook-Air.local>
2025-05-18 03:16:38 +0700

    pertamax

--- modules/ec2_elastic/outputs.tf
+++ modules/ec2_elastic/outputs.tf
@@ -1,14 +1,19 @@
-output "jumphost_public_ip" {
+output "elastic_private_ip" {
     description = "Public IP"
-    value = aws_instance.jumpserver.public_ip
+    value = aws_instance.elastic.private_ip
 }
 
-output "jumphost_private_ip" {
+output "kibana_private_ip" {
     description = "Private IP"
-    value = aws_instance.jumpserver.private_ip
+    value = aws_instance.kibana.private_ip
 }
 
-output "jumphost_id" {
+output "elastic_id" {
     description = "Instance ID"
-    value = aws_instance.jumpserver.id
+    value = aws_instance.elastic.id
+}
+
+output "kibana_id" {
+    description = "Instance ID"
+    value = aws_instance.kibana.id
 }
--- modules/ec2_pam/outputs.tf
+++ modules/ec2_pam/outputs.tf
@@ -1,14 +1,9 @@
-output "jumphost_public_ip" {
-    description = "Public IP"
-    value = aws_instance.jumpserver.public_ip
-}
-
-output "jumphost_private_ip" {
+output "pam_private_ip" {
     description = "Private IP"
-    value = aws_instance.jumpserver.private_ip
+    value = aws_instance.pam.private_ip
 }
 
-output "jumphost_id" {
+output "pam_id" {
     description = "Instance ID"
-    value = aws_instance.jumpserver.id
+    value = aws_instance.pam.id
 }
